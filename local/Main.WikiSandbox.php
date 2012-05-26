9e9aa2c5dd17e5f015ff148dd8841861b8a1406a
Schlaefer <openmail+sourcecode@siezi.com>
2012-05-26 08:26:04 +0200

    Sandkasten Konfiguration ohne erkennbaren Effekt entfernt

--- local/Main.WikiSandbox.php
+++
@@ -1,4 +0,0 @@
-<?php
-$MyHeader=' <img src="http://apfelwiki.de/briefkasten/Main/sandbox_gross_locked.gif" alt="Sandkastenschippe" /> <hr /> Hier k&ouml;nnt ihr einfach rumspielen. Wie in einem Sandkasten. <hr />';
-$Title='Sandkasten';
-?>
