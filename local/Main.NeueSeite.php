34eeef4425763074b6456b09356c86e0c40b75e2
Schlaefer <openmail+sourcecode@siezi.com>
2012-05-26 08:10:52 +0200

    removed unused form definition createpage

--- local/Main.NeueSeite.php
+++ local/Main.NeueSeite.php
@@ -1,16 +1,3 @@
 <?php
-include_once("$FarmD/cookbook/newpageform.php");
-
-if ($_GET['createfrom']!="") {
-   $pagename=MakePageName($_GET['createfrom'],$pagename);
-}
-$CreatePageForm='<form action="'.$ScriptUrl.'">
-  <input type="text" name="n" value="NameOfThePage" />
-  <input type="hidden" name="action" value="edit" />
-  <input type="hidden" name="createfrom" value="'.$pagename.'" />
-  <input type="submit" name="submit" value="Create" />
-</form>';
-Markup('createpage','directives','/\(\:createpage\:\)/e',
-      "Keep('$CreatePageForm')");
-      
+  include_once("$FarmD/cookbook/newpageform.php");
 ?>
